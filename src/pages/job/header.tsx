5b17e438683f0d7160712f3070b3aef5779b844b
yangzy01 <yangzy01@mingyuanyun.com>
2024-10-11 16:16:51 +0800

    feat: 职位列表页面，头部实现

---
+++ src/pages/job/header.tsx
@@ -0,0 +1,128 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import Icons from 'react-native-vector-icons/Ionicons';
+import SizeBox from '../../components/size-box';
+
+const styles = StyleSheet.create({
+  headerTop: {
+    paddingHorizontal: 18,
+    paddingTop: 4,
+    marginTop: StatusBar.currentHeight, // 内容从状态栏下面开始
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 23,
+    fontWeight: 'bold',
+  },
+  headerTopRight: {
+    flexDirection: 'row',
+  },
+  icon: {
+    fontWeight: 'bold',
+    fontSize: 28,
+  },
+  headerBottom: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 18,
+    paddingTop: 14,
+    paddingBottom: 8,
+  },
+  bottomRow: {
+    flexDirection: 'row',
+  },
+  filterBtn: {
+    position: 'relative',
+    backgroundColor: '#F5F5F5',
+    paddingVertical: 5,
+    paddingLeft: 10,
+    paddingRight: 14,
+  },
+  filterTxt: {
+    color: '#5E5E5E',
+  },
+  triangle: {
+    position: 'absolute',
+    right: 4,
+    bottom: 5,
+    width: 0,
+    height: 0,
+    backgroundColor: 'transparent',
+    borderStyle: 'solid',
+    borderRightWidth: 6,
+    borderTopWidth: 6,
+    borderRightColor: 'transparent',
+    borderTopColor: '#C8C6C7',
+    transform: [{rotate: '180deg'}],
+  },
+});
+
+function JobHeader(): React.JSX.Element {
+  return (
+    <>
+      <LinearGradient
+        colors={['#6AD9D8', '#ACEAEA', 'white']}
+        start={{x: 0.5, y: 0}} // 渐变起点
+        end={{x: 0.5, y: 0.5}} // 渐变终点
+        locations={[0, 0.5, 1]} // 每种颜色对应的百分比位置
+      >
+        <StatusBar
+          barStyle="dark-content" // enum('default', 'light-content', 'dark-content')
+          translucent={true} // 应用会延伸到状态栏之下绘制
+          backgroundColor="transparent"
+        />
+        <SafeAreaView>
+          {/* 顶部 */}
+          <View style={styles.headerTop}>
+            <Text style={styles.title}>前端开发工程师</Text>
+            <View style={styles.headerTopRight}>
+              <Icons name="add" style={styles.icon} />
+              <SizeBox width={18} />
+              <Icons name="search" style={styles.icon} />
+            </View>
+          </View>
+          {/* 底部 */}
+          <View style={styles.headerBottom}>
+            <View style={styles.bottomRow}>
+              <TouchableOpacity onPress={() => {}}>
+                <Text>全部</Text>
+              </TouchableOpacity>
+              <SizeBox width={20} />
+              <TouchableOpacity onPress={() => {}}>
+                <Text>附近</Text>
+              </TouchableOpacity>
+              <SizeBox width={20} />
+              <TouchableOpacity onPress={() => {}}>
+                <Text>最新</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.bottomRow}>
+              <View style={styles.filterBtn}>
+                <Text style={styles.filterTxt}>深圳</Text>
+                <View style={styles.triangle} />
+              </View>
+              <SizeBox width={10} />
+              <View style={styles.filterBtn}>
+                <Text style={styles.filterTxt}>筛选</Text>
+                <View style={styles.triangle} />
+              </View>
+            </View>
+          </View>
+        </SafeAreaView>
+      </LinearGradient>
+    </>
+  );
+}
+
+export default JobHeader;
--- src/pages/job/index.tsx
+++ src/pages/job/index.tsx
@@ -1,64 +1,51 @@
 import React from 'react';
 import {
-  // Text,
-  // View,
-  Button,
+  StyleSheet,
+  Text,
+  View,
+  // Button,
   FlatList,
+  // SafeAreaView,
+  TouchableOpacity,
 } from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+// import {useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../../types/global';
+import Header from './header';
 
-const data = [
-  {id: '1', title: 'Item 1'},
-  {id: '2', title: 'Item 2'},
-  {id: '3', title: 'Item 3'},
-  {id: '4', title: 'Item 3'},
-  {id: '5', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-  {id: '3', title: 'Item 3'},
-];
+const data = Array.from({length: 100}, (_, index) => ({
+  id: index + 1, // 生成从1开始的唯一ID
+  title: `Title ${index + 1}`,
+}));
 
 export type HomeScreenNavigationProp = NativeStackNavigationProp<
   RootStackParamList,
   'Home'
 >;
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  item: {
+    backgroundColor: '#fff',
+    padding: 20,
+    marginHorizontal: 10,
+    marginTop: 10,
+    borderRadius: 5,
+  },
+});
+
+const ItemInfo = ({info}: {info: any}) => {
+  return (
+    <TouchableOpacity style={styles.item} onPress={() => {}}>
+      <Text>{info.title}</Text>
+    </TouchableOpacity>
+  );
+};
+
 function Index(): React.JSX.Element {
-  const navigation = useNavigation<HomeScreenNavigationProp>();
+  // const navigation = useNavigation<HomeScreenNavigationProp>();
 
   // const handlePress = () => {
   //   // navigation.push('JobDetail');
@@ -66,17 +53,17 @@ function Index(): React.JSX.Element {
   // };
 
   return (
-    <FlatList
-      data={data}
-      renderItem={({item}) => (
-        // <Text onPress={() => handlePress}>{item.title}</Text>
-        <Button
-          title={item.title}
-          onPress={() => navigation.navigate('JobDetail')}
-        />
-      )}
-      keyExtractor={() => Math.random().toString(36)}
-    />
+    <View style={styles.container}>
+      <Header />
+      <FlatList
+        data={data}
+        renderItem={({item}) => (
+          // <Text onPress={() => handlePress}>{item.title}</Text>
+          <ItemInfo info={item} />
+        )}
+        keyExtractor={() => Math.random().toString(36)}
+      />
+    </View>
   );
 }
 
